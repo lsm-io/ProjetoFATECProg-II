82aaad811c4edf1f352349a2888eef8ec38aed87
lsm-io <lucassmiranda93@gmail.com>
2024-05-16 16:25:25 -0300

    modified:   __pycache__/dbutils.cpython-39.pyc modified:   main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import tkinter as tk
 import ttkbootstrap as ttk
-from tkinter import messagebox
+from tkinter import *
 from dbutils import *
+from tkinter import messagebox
 
 # função que abre a janela de consulta da tabela
 def open_table_window():
@@ -19,7 +20,7 @@ def open_table_window():
     search_entry.bind("<Return>", outer_func)
     search_entry.pack(side='top', fill='x', pady=5)
 
-    search_button = ttk.Button(table_window, text='Pesquisar', command = search_table)
+    search_button = ttk.Button(table_window, text='Pesquisar', command = search_table, bootstyle="info")
     search_button.pack(side='top', pady=5)
 
     table = ttk.Treeview(table_window, columns=('id', 
@@ -28,7 +29,7 @@ def open_table_window():
                                                 'marca', 
                                                 'quantidade', 
                                                 'preço'), 
-                                                show='headings')
+                                                show='headings', bootstyle = 'info')
     table.heading('id', text='ID')
     table.heading('nome', text='Nome')
     table.heading('categoria', text='Categoria')
@@ -121,7 +122,7 @@ def open_add_window():
 def open_change_window():
     change_window = ttk.Toplevel(window)
     change_window.title('Modificar produto')
-    change_window.geometry('550x600')
+    change_window.geometry('550x380')
     change_window.grab_set()
     titulo = ttk.Label(change_window, text = 'Modificar Produto', font = 'FiraCode 24 bold')
     titulo.pack()
@@ -134,49 +135,63 @@ def open_change_window():
     lookup_button = ttk.Button(input_frame, text = 'Pesquisar')
     lookup_button.pack(side = 'left', padx = 5)
     input_frame.pack()
-    
-    id_add_string = ttk.StringVar()
-    id_label = ttk.Label(change_window, text = 'ID: ')
-    id_label.pack(side='top', fill='x', pady=5)
-    add_id = ttk.Entry(change_window, width = 45, textvariable = id_add_string)
-    add_id.pack(side='top', pady=5)
-    id_button = ttk.Button(change_window, width = 10, text = 'Modificar')
-    id_button.pack(side='top')
+    padding = ttk.Label(change_window)
+    padding.pack()
+    id_frame = ttk.Frame(change_window)
+    id_change_string = ttk.StringVar()
+    id_label = ttk.Label(id_frame, text = 'ID: ')
+    id_label.pack(side='left', fill='x', pady=5, padx = 26)
+    change_id = ttk.Entry(id_frame, width = 45, textvariable = id_change_string)
+    change_id.pack(side='left', pady=5)
+    id_button = ttk.Button(id_frame, width = 10, text = 'Modificar')
+    id_button.pack(side='left', padx = 10)
+    id_frame.pack()
+    nome_frame = ttk.Frame(change_window)
     nome_add_string = ttk.StringVar()
-    nome_label = ttk.Label(change_window, text = 'Nome: ')
-    nome_label.pack(side='top', fill='x', pady=5)
-    add_nome = ttk.Entry(change_window, width = 45, textvariable = nome_add_string)
-    add_nome.pack(side='top', pady=5)
-    nome_button = ttk.Button(change_window, width = 10, text = 'Modificar')
-    nome_button.pack(side='top')
+    nome_label = ttk.Label(nome_frame, text = 'Nome: ')
+    nome_label.pack(side='left', fill='x', pady=5, padx = 15)
+    add_nome = ttk.Entry(nome_frame, width = 45, textvariable = nome_add_string)
+    add_nome.pack(side='left', pady=5)
+    nome_button = ttk.Button(nome_frame, width = 10, text = 'Modificar')
+    nome_button.pack(side='left', padx = 10)
+    nome_frame.pack()
+    categoria_frame = ttk.Frame(change_window)
     categoria_add_string = ttk.StringVar()
-    categoria_label = ttk.Label(change_window, text = 'Categoria: ')
-    categoria_label.pack(side='top', fill='x', pady=5)
-    add_categoria = ttk.Entry(change_window, width = 45, textvariable = categoria_add_string)
-    add_categoria.pack(side='top', pady=5)
-    categoria_button = ttk.Button(change_window, width = 10, text = 'Modificar')
-    categoria_button.pack(side='top')
+    categoria_label = ttk.Label(categoria_frame, text = 'Categoria: ')
+    categoria_label.pack(side='left', fill='x', pady=5, padx = 6)
+    add_categoria = ttk.Entry(categoria_frame, width = 45, textvariable = categoria_add_string)
+    add_categoria.pack(side='left', pady=5)
+    categoria_button = ttk.Button(categoria_frame, width = 10, text = 'Modificar')
+    categoria_button.pack(side='left', padx = 10)
+    categoria_frame.pack()
+    marca_frame = ttk.Frame(change_window)
     marca_add_string = ttk.StringVar()
-    marca_label = ttk.Label(change_window, text = 'Marca: ')
-    marca_label.pack(side='top', fill='x', pady=5)
-    add_marca = ttk.Entry(change_window, width = 45, textvariable = marca_add_string)
-    add_marca.pack(side='top', pady=5)
-    marca_button = ttk.Button(change_window, width = 10, text = 'Modificar')
-    marca_button.pack(side='top')
+    marca_label = ttk.Label(marca_frame, text = 'Marca: ')
+    marca_label.pack(side='left', fill='x', pady=5, padx = 15)
+    add_marca = ttk.Entry(marca_frame, width = 45, textvariable = marca_add_string)
+    add_marca.pack(side='left', pady=5)
+    marca_button = ttk.Button(marca_frame, width = 10, text = 'Modificar')
+    marca_button.pack(side='left', padx = 10)
+    marca_frame.pack()
+    quantidade_frame = ttk.Frame(change_window)
     quantidade_add_string = ttk.StringVar()
-    quantidade_label = ttk.Label(change_window, text = 'Quantidade: ')
-    quantidade_label.pack(side='top', fill='x', pady=5)
-    add_quantidade = ttk.Entry(change_window, width = 45, textvariable = quantidade_add_string)
-    add_quantidade.pack(side='top', pady=5)
-    quantidade_button = ttk.Button(change_window, width = 10, text = 'Modificar')
-    quantidade_button.pack(side = 'top')
+    quantidade_label = ttk.Label(quantidade_frame, text = 'Quantidade: ')
+    quantidade_label.pack(side='left', fill='x', pady=5)
+    add_quantidade = ttk.Entry(quantidade_frame, width = 45, textvariable = quantidade_add_string)
+    add_quantidade.pack(side='left', pady=5)
+    quantidade_button = ttk.Button(quantidade_frame, width = 10, text = 'Modificar')
+    quantidade_button.pack(side = 'left', padx = 10)
+    quantidade_frame.pack()
+    preco_frame = ttk.Frame(change_window)
     preco_add_string = ttk.StringVar()
-    preco_label = ttk.Label(change_window, text = 'Preço: ')
-    preco_label.pack(side='top', fill='x', pady=5)
-    add_preco = ttk.Entry(change_window, width = 45, textvariable = preco_add_string)
-    add_preco.pack(side='top', pady=5)
-    preco_button = ttk.Button(change_window, width = 10, text = 'Modificar')
-    preco_button.pack(side = 'top')
+    preco_label = ttk.Label(preco_frame, text = 'Preço: ')
+    preco_label.pack(side='left', fill='x', pady=5, padx = 17)
+    add_preco = ttk.Entry(preco_frame, width = 45, textvariable = preco_add_string)
+    add_preco.pack(side='left', pady=5)
+    preco_button = ttk.Button(preco_frame, width = 10, text = 'Modificar')
+    preco_button.pack(side = 'left', padx = 10)
+    preco_frame.pack()
+    
 
 
 def delete_outer_func():
@@ -246,10 +261,6 @@ def open_delete_window():
 
     delete_button = ttk.Button(delete_window, text='Excluir', command = delete_prod)
     delete_button.pack(side='top', pady=5)
-
-
-def open_custom_window():
-    d = 1
            
      
 def open_main_window():
@@ -257,23 +268,23 @@ def open_main_window():
     global window
     window = ttk.Toplevel(login_window)
     window.title('Menu Principal')
-    window.geometry('450x400')
+    window.geometry('400x300')
+    window_frame = ttk.Frame(window)
 
     # mensagem
-    bem_vindo = tk.Label(master = window, text = 'Bem vindo', font = 'FiraCode 24 bold')
-    bem_vindo.place(relx=0.5, rely=0.1, anchor='center')
+    bem_vindo = tk.Label(master = window_frame, text = 'Bem vindo', font = 'FiraCode 24 bold')
+    bem_vindo.grid(row = 0, column = 1)
 
     # botões
-    consultar_tabela_botao = ttk.Button(master = window, text = 'Consultar tabela', command = open_table_window, width = 50)
-    consultar_tabela_botao.place(relx=0.5, rely=0.3, anchor='center')
-    adicionar_produto_botao = ttk.Button(master = window, text = 'Adicionar produto', command = open_add_window, width = 50)
-    adicionar_produto_botao.place(relx=0.5, rely=0.4, anchor='center')
-    modificar_produto_botao = ttk.Button(master = window, text = 'Modificar produto', command = open_change_window, width = 50)
-    modificar_produto_botao.place(relx=0.5, rely=0.5, anchor='center')
-    excluir_produto_botao = ttk.Button(master = window, text = 'Excluir produto', command = open_delete_window, width = 50)
-    excluir_produto_botao.place(relx=0.5, rely=0.6, anchor='center')
-    consulta_custom_botao = ttk.Button(master = window, text = 'Consulta personalizada', command = open_custom_window, width = 50)
-    consulta_custom_botao.place(relx=0.5, rely=0.7, anchor='center')
+    consultar_tabela_botao = ttk.Button(master = window_frame, text = 'Consultar tabela', command = open_table_window, width = 50, bootstyle="info")
+    consultar_tabela_botao.grid(row = 1, column = 1, pady = 10)
+    adicionar_produto_botao = ttk.Button(master = window_frame, text = 'Adicionar produto', command = open_add_window, width = 50, bootstyle="info")
+    adicionar_produto_botao.grid(row = 2, column = 1, pady = 10)
+    modificar_produto_botao = ttk.Button(master = window_frame, text = 'Modificar produto', command = open_change_window, width = 50, bootstyle="info")
+    modificar_produto_botao.grid(row = 3, column = 1, pady = 10)
+    excluir_produto_botao = ttk.Button(master = window_frame, text = 'Excluir produto', command = open_delete_window, width = 50, bootstyle="info")
+    excluir_produto_botao.grid(row = 4, column = 1, pady = 10)
+    window_frame.pack()
     login_window.withdraw()
     window.protocol("WM_DELETE_WINDOW", on_closing)
 
@@ -302,7 +313,7 @@ def login():
 
 
 # janela de login
-login_window = ttk.Window(themename = 'flatly')
+login_window = ttk.Window(themename = 'darkly')
 login_window.title('Login')
 login_window.geometry('300x200')
 login_window.grab_set()
@@ -312,15 +323,16 @@ counter = 2
 login_str = ttk.StringVar()
 senha_str = ttk.StringVar()
 login_label = ttk.Label(login_frame, text = 'Login: ')
-login_entry = ttk.Entry(login_frame, width = 20, textvariable = login_str)
+login_entry = ttk.Entry(login_frame, width = 20, textvariable = login_str, bootstyle="dark")
 senha_label = ttk.Label(login_frame, text = 'Senha: ')
-senha_entry = ttk.Entry(login_frame, width = 20, show = '*', textvariable = senha_str)
+senha_entry = ttk.Entry(login_frame, width = 20, show = '*', textvariable = senha_str, bootstyle="dark")
 senha_entry.bind("<Return>", outer_login)
-login_button = ttk.Button(login_frame, text = 'Entrar', command = login)
+login_button = ttk.Button(login_frame, text = 'Entrar', command = login, bootstyle="info")
 login_label.grid(row = 0, column = 1)
 login_entry.grid(row = 1, column = 1)
+login_entry.focus_set()
 senha_label.grid(row = 2, column = 1)
-senha_entry.grid(row = 3, column = 1)
-login_button.grid(row = 4, column = 1)
+senha_entry.grid(row = 3, column = 1,)
+login_button.grid(row = 5, column = 1, pady = 10)
 login_frame.pack()
 login_window.mainloop()
